cef42bb477d40ac92abef971cc8777a1f22c574b
Alberto Fuentes <fuentessec@gmail.com>
2018-10-03 19:44:00 +0200

    Reading arguments

--- main.cpp
+++ main.cpp
@@ -42,15 +42,52 @@
 typedef unsigned long long timestamp_t;
 using namespace std;
 
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+    vm_usage     = 0.0;
+    resident_set = 0.0;
+
+    // the two fields we want
+    unsigned long vsize;
+    long rss;
+    {
+        std::string ignore;
+        std::ifstream ifs("/proc/self/stat", std::ios_base::in);
+        ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> vsize >> rss;
+    }
+
+    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+    vm_usage = vsize / 1024.0;
+    resident_set = rss * page_size_kb;
+}
+
 static timestamp_t get_timestamp() {
     struct timeval now;
     gettimeofday(&now, NULL);
     return now.tv_usec + (timestamp_t) now.tv_sec * 1000000;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     timestamp_t begin;
     double diff;
+    
+    /// read arguments
+    if (argc != 4){
+        printf ("Usage: main NumThreds TraceElementPrecision floatPrecision");
+        printf ("Numthreads = number of threads for parallel calculations");
+        printf ("TraceElementPrecision = 1->8-bits, 2->16-bits, 3->32-bits");
+        printf ("floatPrecision = 1->float(32-bits), 2->double(64bits)");
+        printf ("NOTE: program does not do input validation (put correct values)");
+        return -1;
+    }
+    
+    /// set values given as arguments
+    setenv("NTHREADS", argv[1],1);
+    int tracePrecision = atoi (argv[2]);
+    int floatPrecision = atoi (argv[3]);
+    
 
     TraceSet traceSet = TraceSet();
     Trace *aux;
@@ -161,26 +198,6 @@ int main() {
         
         res->toPng("correlation.png");
     }
+    
     return 0;
-}
-
-void process_mem_usage(double& vm_usage, double& resident_set)
-{
-    vm_usage     = 0.0;
-    resident_set = 0.0;
-
-    // the two fields we want
-    unsigned long vsize;
-    long rss;
-    {
-        std::string ignore;
-        std::ifstream ifs("/proc/self/stat", std::ios_base::in);
-        ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-                >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-                >> ignore >> ignore >> vsize >> rss;
-    }
-
-    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
-    vm_usage = vsize / 1024.0;
-    resident_set = rss * page_size_kb;
 }
