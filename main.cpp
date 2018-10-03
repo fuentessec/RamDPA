27df5585a57dbebc2ccf915090edc4b2a5065fe5
Alberto Fuentes <fuentessec@gmail.com>
2018-10-03 19:28:57 +0200

    Added memory consumption measurement

--- main.cpp
+++ main.cpp
@@ -146,6 +146,11 @@ int main() {
         printf("Time -> %f\n", diff);
 
 
+        // Check memory usage...
+        double vm, rss;
+        process_mem_usage(vm, rss);
+        printf("VM: %f; RSS: %f; Total: \n", vm, rss, vm+rss);
+        
         int keyPos, tracePos;
         double valuePos = res->getMaxValue(keyPos, tracePos);
 
@@ -157,4 +162,25 @@ int main() {
         res->toPng("correlation.png");
     }
     return 0;
+}
+
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+    vm_usage     = 0.0;
+    resident_set = 0.0;
+
+    // the two fields we want
+    unsigned long vsize;
+    long rss;
+    {
+        std::string ignore;
+        std::ifstream ifs("/proc/self/stat", std::ios_base::in);
+        ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> vsize >> rss;
+    }
+
+    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+    vm_usage = vsize / 1024.0;
+    resident_set = rss * page_size_kb;
 }
