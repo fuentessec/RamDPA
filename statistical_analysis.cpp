ef0f2a3333c11afc6bae58a72552c1ebeca88e5e
fuentessec <fuentessec@gmail.com>
2018-10-10 12:24:15 +0200

    Changed for ordering at statistical analysis (it is ok), but coorelation does not work well

--- statistical_analysis.cpp
+++ statistical_analysis.cpp
@@ -4,9 +4,9 @@
 
 #define MULTITHREAD
 
-StatisticalAnalysis::StatisticalAnalysis (ConsumptionMatrix *consumptionSet, 
-                                          TraceSet *traceSet, 
-                                          bool doublePrecision){
+StatisticalAnalysis::StatisticalAnalysis(ConsumptionMatrix *consumptionSet,
+        TraceSet *traceSet,
+        bool doublePrecision) {
     this->traceSet = traceSet;
     this->consumptionSet = consumptionSet;
     this->keySize = consumptionSet->getSizeKeyHypothesis();
@@ -14,27 +14,27 @@ StatisticalAnalysis::StatisticalAnalysis (ConsumptionMatrix *consumptionSet,
     this->doublePrecision = doublePrecision;
 }
 
-
-TraceSet* StatisticalAnalysis::getTraceSet (){
+TraceSet* StatisticalAnalysis::getTraceSet() {
     return traceSet;
 }
-ConsumptionMatrix* StatisticalAnalysis::getConsumptionSet (){
+
+ConsumptionMatrix* StatisticalAnalysis::getConsumptionSet() {
     return consumptionSet;
 }
 
-ResultMatrix* StatisticalAnalysis::doStatisticalAnalysis (){
-    if (doublePrecision){
+ResultMatrix* StatisticalAnalysis::doStatisticalAnalysis() {
+    if (doublePrecision) {
         res = new ResultMatrix64(keySize, traceSize);
-    }else{
+    } else {
         res = new ResultMatrix32(keySize, traceSize);
     }
-    
+
 #ifdef MULTITHREAD
     int num_threads = 1; // default value if NTHREADS environment variable is not defined
 
     if (getenv("NTHREADS"))
         num_threads = atoi(getenv("NTHREADS")); // number of threads as specified by NTHREADS.
-    
+
     // create threads
     vector <thread> threads;
     for (int i = 0; i < num_threads; i++) {
@@ -49,21 +49,28 @@ ResultMatrix* StatisticalAnalysis::doStatisticalAnalysis (){
     threads.clear();
 #else
     for (int k = 0; k < keySize; k++) {
-        for (int t = 0; t < traceSize; t++) { 
+        for (int t = 0; t < traceSize; t++) {
             calculate(k, t);
         }
     }
 #endif
-        
+
     return res;
 }
 
-void StatisticalAnalysis::threadCalculation(int totalThreads, int numThread) { 
-    for (int k = numThread; k < keySize; k+=totalThreads) {
-        for (int t = 0; t < traceSize; t++) { 
+void StatisticalAnalysis::threadCalculation(int totalThreads, int numThread) {
+    for (int t = 0; t < traceSize; t += totalThreads) {
+        for (int k = numThread; k < keySize; k++) {
             calculate(k, t);
         }
     }
-      
-     
-}
+
+
+//    for (int t = 0; t < traceSize; t += totalThreads) {
+//        for (int k = numThread; k < keySize; k++) {
+//            calculate(k, t);
+//        }
+//    }
+
+
+    }
